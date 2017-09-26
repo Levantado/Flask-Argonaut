efd792e7991a4d9eede79ac4e1307b70f859b429
Anton Oleynik <levantado@me.com>
2017-09-26 23:44:47 +0400

    Small fixes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup
 
 setup(name='Flask-Argonaut',
-      version='0.18',
+      version='0.19',
       description='Flask extension use hashing data with Argon2',
       author='Anton Oleynik',
       author_email='levantado@me.com',
