262852149265961380703d15bb6e4a02646b08c1
Anton Oleynik <levantado@me.com>
2017-09-26 23:11:29 +0400

    Add tag

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@
 from setuptools import setup
 
 setup(name='Flask-Argonaut',
-      version='0.14',
+      version='0.16',
       description='Flask extension use hashing data with Argon2',
       author='Anton Oleynik',
       author_email='levantado@me.com',
       license='BSD',
+      urls='https://github.com/Levantado/Flask-Argonaut',
+      download_url = 'https://github.com/Levantado/Flask-Argonaut/archive/master.zip',
       packages=['flask_argonaut'],
       platforms='any',
       install_requires=['Flask', 'argon2'],
