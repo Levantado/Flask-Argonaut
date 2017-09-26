1afb3426c53749cd7b08a17e0a8cb3a098ec057c
Anton Oleynik <levantado@me.com>
2017-09-26 23:22:13 +0400

    Add download tarballs

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@
 from setuptools import setup
 
 setup(name='Flask-Argonaut',
-      version='0.16',
+      version='0.17',
       description='Flask extension use hashing data with Argon2',
       author='Anton Oleynik',
       author_email='levantado@me.com',
       license='BSD',
       urls='https://github.com/Levantado/Flask-Argonaut',
-      download_url = 'https://github.com/Levantado/Flask-Argonaut/archive/master.zip',
+      download_url = 'https://github.com/Levantado/Flask-Argonaut/tarball/v0.17',
       packages=['flask_argonaut'],
       platforms='any',
       install_requires=['Flask', 'argon2'],
